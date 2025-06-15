c639672f3ef429b90ea130a82ea934af0510da38
root <root@sdl63310.labs.teradata.com>
2025-06-15 06:52:55 -0700

    ttubasesuite 20.00.24.00

--- Casks/ttubasesuite.rb
+++ Casks/ttubasesuite.rb
@@ -1,6 +1,6 @@
 cask "ttubasesuite" do
-  version "20.00.23.00,2025-05"
-  sha256 "24693ad17a1568ad0db1318ac59d394b36fca2e377fe01b6d28cb84b81d53b50"
+  version "20.00.24.00,2025-06"
+  sha256 "a31103e3087e4436282ad66c436f2ff8c72339708c4ec0090a96435d58eb9b61"
 
   url "https://downloads.teradata.com/sites/default/files/#{version.csv.second}/TeradataToolsAndUtilities-macosx-brew-#{version.csv.first}.tar"
   name "Teradata Tools and Utilities"
